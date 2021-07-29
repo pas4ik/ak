7fcff38b2181e12966bad940b1761f856974a7d4
pas4ik <pas4ik@gmail.com>
2021-07-29 08:36:05 +0300

    Update main.cpp

--- ArdNANO_serv_test/src/main.cpp
+++ ArdNANO_serv_test/src/main.cpp
@@ -7,25 +7,25 @@
 #define START_BYTE  'S'
 #define START       1
 #define STOP        2
+#define PIN_GERKON  PIN6
+#define TIMEOUT_STOP_WO_GERK  3000 // ms
 
 Servo serv;
 SoftwareSerial swserial1(2, 3);
 uint8_t run = 0;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  swserial1.begin(9600); // interface for radio control
-  serv.attach(7);     // servo pin
-  swserial1.print("Hello");
+  pinMode(LED_BUILTIN, OUTPUT); // LED
+  pinMode(PIN_GERKON, INPUT_PULLUP); // input zero gerkon
+  swserial1.begin(9600);    // interface for radio control
+  serv.attach(7);           // servo pin
+  serv.write(SERVO_STOP);
 }
 
 void loop() {
-  int rx_temp = 0, com;
-  uint8_t UART_Rx_buf[10], rx_cnt = 0;
-//  serv.write(SERVO_GO);
-//  delay(1000);
-  serv.write(SERVO_STOP);
-//  delay(1000);
+  static int rx_temp = 0, com = 0;
+  static uint8_t UART_Rx_buf[10], rx_cnt = 0;
+  static unsigned long time_com_stop = 0;
   
   if (swserial1.available() > 0)
   {
@@ -34,12 +34,10 @@ void loop() {
     {
       if (rx_temp == START_BYTE)
       UART_Rx_buf[rx_cnt++] = (uint8_t)rx_temp;
-      swserial1.print(byte(rx_temp));
     } else
     {
       UART_Rx_buf[rx_cnt++] = (uint8_t)rx_temp;
-      swserial1.print(byte(rx_temp));
-      if (rx_cnt == 3)
+      if (rx_cnt >= 3)
       {
         if ((UART_Rx_buf[0] == START_BYTE) && (UART_Rx_buf[1] == 'T') && (UART_Rx_buf[2] == 'A'))
         {
@@ -57,16 +55,37 @@ void loop() {
   if (com == START)
   {
     digitalWrite(LED_BUILTIN, HIGH);
+    serv.write(SERVO_GO);
+    run = 1;
+    com = 0;
+    time_com_stop = 0;
   } else
   if (com == STOP)
   {
-    digitalWrite(LED_BUILTIN, LOW);
+    if (time_com_stop == 0) time_com_stop = millis();
+
+    if (digitalRead(PIN_GERKON) == 0)
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+      serv.write(SERVO_STOP);
+      run = 0;
+      com = 0;
+      //swserial1.write('g');
+    } else
+    if (millis() > time_com_stop + TIMEOUT_STOP_WO_GERK)
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+      serv.write(SERVO_STOP);
+      run = 0;
+      com = 0;
+    }
   }
 
   if (run == 1)
   {
 
   } else
+  if (run == 0)
   {
     
   }
